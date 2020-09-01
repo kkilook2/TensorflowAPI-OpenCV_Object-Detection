1a0bfae6523737efe93b3c00f879a839878a51ea
kkilook <44021629+kkilook2@users.noreply.github.com>
2020-09-01 16:38:12 +0900

    Update opencv_video_capture_example.py

--- opencv_video_capture_example.py
+++ opencv_video_capture_example.py
@@ -1,1 +1,18 @@
+##BASIC CODE##
+import cv2
 
+capture = cv2.VideoCapture(0)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+while(True):
+    ret, frame = capture.read()
+
+    if ret == True :
+        cv2.imshow("Video FRame", frame)
+        if cv2.waitKey(33) == ord('q'): break
+    else :
+        break
+
+capture.release()
+cv2.destroyAllWindows()
